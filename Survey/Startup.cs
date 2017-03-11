d8cbde99d88bfcadd78b34aa90d5eb386299cb81
Olena Sysoyeva <osysoyeva@abilis.ca>
2017-03-11 14:41:55 -0500

    initial commit

---
+++ Survey/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Survey.Startup))]
+namespace Survey
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
